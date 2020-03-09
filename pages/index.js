b0b6321281508e162a0e1eb2028298bab49b7f89
Conrad Johnston <conrad@darnoc.co.nz>
2020-03-10 11:56:04 +1300

    Create index page to demo each step. Create file for your-details.

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default () => {
+  return (
+    <ul>
+      <ol>
+        <li>
+          <a href="/begin" target="_blank">
+            Begin
+          </a>
+        </li>
+        <li>
+          <a href="/your-details" target="_blank">
+            Your details
+          </a>
+        </li>
+      </ol>
+    </ul>
+  );
+};
---
+++ pages/your-details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function YourDetails() {
+  return <>Your Details</>;
+}
+
+export default YourDetails;
