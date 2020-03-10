60ac3640fe0c96cee50ad6639e0adc61b27b6c5b
Conrad Johnston <conrad@darnoc.co.nz>
2020-03-10 17:20:47 +1300

    Install and configure Formik and Yup, create first field, validation and mock submit method.

--- pages/your-details.js
+++ pages/your-details.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import PageTemplate from "../components/PageTemplate";
+import { Formik, Field } from "formik";
+import * as Yup from "yup";
+
+const ValidationSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .required('Required'),
+});
 
 function YourDetails() {
-  return <>Your Details</>;
+  return (
+    <PageTemplate>
+      <h1 className="h2">Your details</h1>
+      <Formik
+        initialValues={{ firstName: "Conrad" }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+          }, 1000);
+        }}
+        validationSchema={ValidationSchema}
+      >
+        {({ handleSubmit, errors }) => (
+          <form onSubmit={handleSubmit}>
+            <label>
+              First name
+              <Field name="firstName" />
+            </label>
+            {errors.firstName && <div>{errors.firstName}</div>}
+            <button>Save draft</button>
+            <button type="submit">Next</button>
+          </form>
+        )}
+      </Formik>
+    </PageTemplate>
+  );
 }
 
 export default YourDetails;
